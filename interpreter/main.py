f89cef105686aa5f22c9294f6bb57402a308ef08
hamza aziz <haziz2170@gmail.com>
2025-05-04 15:09:05 +0200

    Add interpreter functionality with JSON program execution and basic arithmetic operations.

---
+++ interpreter/main.py
@@ -0,0 +1,57 @@
+import sys, json
+
+def do_add(env, args):
+    assert len(args) == 2
+    left = do(env, args[0])
+    right = do(env, args[1])
+    return left + right
+
+def do_abs(env, args):
+    assert len(args) == 1
+    val = do(env, args[0])
+    return abs(val)
+
+def do_get(env, args):
+    assert len(args) == 1
+    assert isinstance(args[0], str)
+    assert args[0] in env, f"Unknown variable {args[0]}"
+    return env[args[0]]
+
+def do_set(env, args):
+    assert len(args) == 2
+    assert isinstance(args[0], str)
+    value = do(env, args[1])
+    env[args[0]] = value
+    return value
+
+def do_seq(env, args):
+    assert len(args) > 0
+    for item in args:
+        result = do(env, item)
+    return result
+
+OPS = {
+    name.replace("do_", ""): func
+    for (name, func) in globals().items()
+    if name.startswith("do_")
+}
+
+def do(env, expr):
+    if isinstance(expr, int):
+        return expr
+    assert isinstance(expr, list)
+    print(OPS)
+    assert expr[0] in OPS, f"Unknown operation {expr[0]}"
+    func = OPS[expr[0]]
+    return func(env, expr[1:])
+
+def main():
+    assert len(sys.argv) == 2, "Usage: expr.py filename"
+    with open(sys.argv[1], "r") as reader:
+        program = json.load(reader)
+    env= {}
+    result = do(env, program)
+    print(f"=> {result}")
+
+if __name__ == "__main__":
+    main()
