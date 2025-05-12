d91c34392b040e4aee70ce8c5ebf71e4bf486f2c
hamza aziz <haziz2170@gmail.com>
2025-05-12 23:20:09 +0200

    Implement file backup functionality and manifest creation in Archive class.

--- file/file.py
+++ file/file.py
@@ -1,8 +1,22 @@
 import glob
 from pathlib import Path
 import hashlib
-HASH_LEN = 16
+from time import time
+import csv
+import shutil
 
+HASH_LEN = 16
+INDEX_LEN = 10
+
+def current_time():
+    return f"{time.time()}".split(".")[0]
+
+def get_index(backup_dir)->str:
+    files = glob.glob("**/*.csv", root_dir=backup_dir, recursive=True)
+    files_index = [int(file.split(".csv")[0]) for file in files]
+    current_index= max(files_index)
+    return '0' * (INDEX_LEN - len(str(current_index + 1))) + str(current_index + 1)
+    
 def hash_all(root):
     result = []
     for name in glob.glob("**/*.*", root_dir=root, recursive=True):
@@ -14,5 +28,50 @@ def hash_all(root):
     return result
 
 
+class Archive:
+    def __init__(self, source_dir):
+        self.source_dir = source_dir
+
+    def backup(self, backup_dir):
+        manifest = hash_all(self.source_dir)
+        timestemp = current_time()
+        self._write_manifest(backup_dir, timestemp, manifest)
+        self._copy_files(self.source_dir, backup_dir, manifest)
+        return manifest
+    
+    def _write_manifest(self, backup_dir, timestemp, manifest):
+        backup_dir = Path(backup_dir)
+        if not backup_dir.exists():
+            backup_dir.mkdir()
+
+        manifest_file = Path(backup_dir, f"{timestemp}.csv")
+        with open(manifest_file, "w") as raw:
+            writer = csv.writer(raw)
+            writer.writerow(["filename", "hash"])
+            writer.writerows(manifest)
+
+    def _copy_files(self, source_dir, backup_dir, manifest):
+        for (file_name, hash_code) in manifest:
+            source_path = Path(source_dir, file_name)
+            backup_path = Path(backup_dir, f"{hash_code}.bck")
+            if not backup_path.exists():
+                shutil.copy(source_path, backup_path)
 
 
+class ArchiveIndex:
+    def __init__(self, source_dir):
+        super().__init__(source_dir)
+
+    def backup(self, backup_dir):
+        manifest = hash_all(self.source_dir)
+        index = self.get_index(backup_dir)
+        self._write_manifest(backup_dir, index, index)
+        self._copy_files(self.source_dir, backup_dir, manifest)
+        return manifest
+    
+
+
+
+
+            
+        
--- file/test.py
+++ file/test.py
@@ -1,21 +1,25 @@
 import pytest
 import pyfakefs
-
-from file import hash_all, HASH_LEN
+import file
+from pathlib import Path
+from file import hash_all, HASH_LEN, Archive
+from unittest.mock import patch
+import os
+from file import get_index
 
 @pytest.fixture
-def our_fs(fs):
+def our_fs_1(fs):
     fs.create_file("a.txt", contents="aaa")
     fs.create_file("b.txt", contents="bbb")
     fs.create_file("sub_dir/c.txt", contents="ccc")
 
-def test_hashing(our_fs):
+def test_hashing(our_fs_1):
     result = hash_all(".")
     expected = {"a.txt", "b.txt", "sub_dir/c.txt"}
     assert {r[0] for r in result} == expected
     assert all(len(r[1]) == HASH_LEN for r in result)
 
-def test_change(our_fs):
+def test_change(our_fs_1):
     original = hash_all(".")
     original = [entry for entry in original if entry[0] == "a.txt"][0]
     with open("a.txt", "w") as writer:
@@ -23,3 +27,36 @@ def test_change(our_fs):
     changed = hash_all(".")
     changed = [entry for entry in changed if entry[0] == "a.txt"][0]
     assert original != changed
+
+
+FILES = {"a.txt": "aaa", "b.txt": "bbb", "sub_dir/c.txt": "ccc"}
+BACKUP_DIR_MANIFEST_FILE = {"0000001.csv": "242324", "0000002.csv": "242325"}
+backup_dir = "/backup"
+
+@pytest.fixture
+def our_fs(fs):
+    for name, contents in FILES.items():
+        fs.create_file(name, contents=contents)
+    if not Path(backup_dir).exists():
+        Path(backup_dir).mkdir()
+    for name, contents in BACKUP_DIR_MANIFEST_FILE.items():
+        fs.create_file(Path(backup_dir, name), contents=contents)
+
+
+def test_our_fs(our_fs):
+    backup_file_name = os.path.join('/backup', "0000001.csv")
+    assert Path(backup_file_name).exists
+
+def test_index(our_fs):
+
+    assert get_index(backup_dir) == "0000000003"
+
+
+def test_nested_example(our_fs):
+    timestamp = 1234
+    with patch("file.current_time", return_value=timestamp):
+        backup = Archive(".")
+        manifest = backup.backup("/backup")
+    assert Path("/backup", f"{timestamp}.csv").exists()
+    for filename, hash_code in manifest:
+        assert Path("/backup", f"{hash_code}.bck").exists()
