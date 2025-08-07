d634893a34d16fcc0b51ed52cac4f85594d9eef2
hamza aziz <haziz2170@gmail.com>
2025-08-07 12:10:43 +0200

    Feat: Introduce DfCol for column-oriented data and add performance benchmarking suite.

--- per/main.py
+++ per/main.py
@@ -1,3 +1,8 @@
+import time 
+
+def all_eq(*values):
+    return (not values) or all(v == values[0] for v in values)
+
 class DataFrame:
     def ncol(self):
         """Report the number of columns."""
@@ -19,8 +24,6 @@ class DataFrame:
 
     def filter(self, func):
         """Select a subset of rows by testing values."""
-
-
 class DfRow(DataFrame):
     def __init__(self, rows):
         assert len(rows) > 0
@@ -57,20 +60,167 @@ class DfRow(DataFrame):
         return DfRow(rows)
     
     def filter(self, func):
-        ''' to filter dataframe row , by func
-            it make a copy of the data
-        '''
-        new_data = []
-        for row in self._data:
-            if func(row):
-                new_data.append(row)
-        return DfRow(new_data)
-    
+        result = [r for r in self._data if func(**r)]
+        return DfRow(result)
 
+    def toColRow(self):
+        """ col format is the following dict with key : value (list)
+            row format is in the forme of : [{k (col), v }]
+        """
+        def get_col_list(k):
+            l = []
+            for r in range(self.nrow()):
+                l.append(self.get(k, r))
+            return l
     
-
+        return DfCol(** {
+            k : get_col_list(k) for k in self.cols()
+        })
+        
+    
 def dict_match(d, prototype):
     if set(d.keys()) != set(prototype.keys()):
         return False
     return all(type(d[k]) == type(prototype[k]) for k in d)
+class DfCol(DataFrame):
+    def __init__(self, **kwargs) -> None:
+        assert len(kwargs) > 0 
+        assert all_eq(len(kwargs[k]) for k in kwargs)
+        for k in kwargs:
+            assert all_eq(type(v) for v in kwargs[k])
+        self._data = kwargs
+    def ncol(self):
+        return len(self._data)
 
+    def nrow(self):
+        n = list(self._data.keys())[0]
+        return len(self._data[n])
+
+    def cols(self):
+        return set(self._data.keys())
+
+    def get(self, col, row):
+        assert col in self._data
+        assert 0 <= row < len(self._data[col])
+        return self._data[col][row]
+
+    def eq(self, other):
+        assert isinstance(other, DataFrame)
+        for n in self._data:
+            if n not in other.cols():
+                return False
+            for i in range(len(self._data[n])):
+                if self.get(n, i) != other.get(n, i):
+                    return False
+        return True
+    
+    def select(self, *names):
+        assert all(n in self._data for n in names)
+        return DfCol(**{n: self._data[n] for n in names})
+    
+    def filter(self, func):
+        result = {n: [] for n in self._data}
+        for i in range(self.nrow()):
+            args = {n: self._data[n][i] for n in self._data}
+            if func(**args):
+                for n in self._data:
+                    result[n].append(self._data[n][i])
+        return DfCol(**result)
+    
+
+
+RANGE = 10
+
+def make_col(nrow, ncol):
+    def _col(n, start):
+        return [((start + i) % RANGE) for i in range(n)]
+    fill = {f"label_{c}": _col(nrow, c) for c in range(ncol)}
+    return DfCol(**fill)
+
+def make_row(nrow, ncol):
+    labels = [f"label_{c}" for c in range(ncol)]
+    def _row(r):
+        return {
+            c: ((r + i) % RANGE) for (i, c) in enumerate(labels)
+        }
+    fill = [_row(r) for r in range(nrow)]
+    return DfRow(fill)
+
+FILTER = 2
+
+
+def time_filter(df):
+    def f(label_0, **args):
+        return label_0 % FILTER == 1
+    start = time.time()
+    df.filter(f)
+    return time.time() - start
+
+
+SELECT = 3
+
+def time_select(df):
+    indices = [i for i in range(df.ncol()) if ((i % SELECT) == 0)]
+    labels = [f"label_{i}" for i in indices]
+    start = time.time()
+    df.select(*labels)
+    return time.time() - start
+
+def make_cold(nrow, ncol):
+    def _col(n, start):
+        return [((start + i) % RANGE) for i in range(n)]
+    fill = {f"label_{c}": _col(nrow, c) for c in range(ncol)}
+    return DfcolD(**fill)
+
+def time_filter_efficient(df):
+    def f(label_0):
+        return label_0 % FILTER == 1
+    start = time.time()
+    df.filter(f)
+    return time.time() - start
+
+def sweep(sizes):
+    result = []
+    for (nrow, ncol) in sizes:
+        df_col = make_col(nrow, ncol)
+        df_row = make_row(nrow, ncol)
+        df_cold = make_cold(nrow, ncol)
+        times = [
+            time_filter(df_col),
+            time_select(df_col),
+            time_filter(df_row),
+            time_select(df_row),
+            time_filter_efficient(df_cold),
+        ]
+        result.append([nrow, ncol, *times])
+    return result
+
+
+from inspect import signature
+class DfcolD(DfCol):
+    """dict of list"""
+    def filter(self, func):
+        params = get_col(func)
+        result = {n: [] for n in self._data}
+        for i in range(self.nrow()):
+            args_for_func = {}
+            for p in params:
+                if p == 'i_row':
+                    args_for_func['i_row'] = i
+                else:
+                    args_for_func[p] = self._data[p][i]
+            if func(**args_for_func):
+                for n in self._data:
+                    result[n].append(self._data[n][i])
+        return DfcolD(**result)
+    
+def get_col(func):
+    sig = signature(func)
+    return list(sig.parameters.keys())
+
+if __name__ == '__main__':
+    sizes = [(10, 10), (100, 100), (1000, 1000), (10000, 100)]
+    results = sweep(sizes)
+    print("nrow\tncol\tfilter col\tselect col\tfilter row\tselect row\tfilter eff")
+    for r in results:
+        print(f"{r[0]}\t{r[1]}\t{r[2]:.6f}\t{r[3]:.6f}\t{r[4]:.6f}\t{r[5]:.6f}\t{r[6]:.6f}")
--- per/test_main.py
+++ per/test_main.py
@@ -1,33 +1,27 @@
-from main import DfRow
+from main import DfRow, DfCol
 
-def test_filter():
-    
-    given = [
-        {
-            "a":4,
-            "b":5
-        },
-        {
-            "a":3,
-            "b":5
-        },
+def odd_even():
+    return DfRow([{"a": 1, "b": 3}, {"a": 2, "b": 4}])
 
-            ]
-    
-    df = DfRow(given)
+def test_filter_row():
+    def odd(a, b):
+        return (a % 2) == 1
 
-    def is_a_column_even(row):
-        if 'a' in row and row['a'] % 2 == 0:
-            return True
-        return False
-    
-    df = df.filter(is_a_column_even)
-        
-    expected = [
-        {
-            "a":4,
-            "b":5
-        }
-            ]
-    
-    assert df.eq(DfRow(expected))
+    df = odd_even()
+    assert df.filter(odd).eq(DfRow([{"a": 1, "b": 3}]))
+
+def test_construct_col_with_two_pairs():
+    df = DfCol(a=[1, 2], b=[3, 4])
+    assert df.get("a", 0) == 1
+    assert df.get("a", 1) == 2
+    assert df.get("b", 0) == 3
+    assert df.get("b", 1) == 4
+
+def test_filter_col():
+    def odd(a, b):
+        return (a % 2) == 1
+
+    df = DfCol(a=[1, 2], b=[3, 4])
+    assert df.filter(odd).eq(DfCol(a=[1], b=[3]))
+
+
